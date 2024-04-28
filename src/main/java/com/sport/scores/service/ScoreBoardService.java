2a5d54cedc8ab96fc1609e1b5f7fdc253079f007
TZ <tomasz.zumbrzycki@gmail.com>
2024-04-28 22:41:02 +0200

    Added javadocs

--- src/main/java/com/sport/scores/service/ScoreBoardService.java
+++ src/main/java/com/sport/scores/service/ScoreBoardService.java
@@ -8,11 +8,33 @@ import java.util.List;
 
 public interface ScoreBoardService {
 
+    /**
+     * Method starts a match and ads it to ongoing games
+     * @param match
+     * @throws InvalidMatchState when match data is incorrect
+     */
     void startMatch(Match match) throws InvalidMatchState;
 
+    /**
+     * Method marks match as finished and removes it from ongoing games
+     * @param match
+     * @throws InvalidMatchState when match data is incorrect
+     */
     void finishMatch(Match match) throws InvalidMatchState;
 
+    /**
+     * Method updates score of ongoing game
+     * @param match
+     * @param homeTeamScore
+     * @param awayTeamScore
+     * @throws InvalidScore when score data is incorrect
+     * @throws InvalidMatchState when match data is incorrect
+     */
     void updateScore(Match match, int homeTeamScore, int awayTeamScore) throws InvalidScore, InvalidMatchState;
 
+    /**
+     * Method returns a list of ongoing games, sorted
+     * @return list of ongoing games, highest total scores are first and if scores are equal, games started first are higher on a list
+     */
     List<Match> getOngoingMatchesOrdered();
 }
