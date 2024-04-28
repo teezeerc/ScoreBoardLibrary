5956c221d7efca50091e00ef08f6708bc06c04e0
TZ <tomasz.zumbrzycki@gmail.com>
2024-04-28 22:19:00 +0200

    Test for sorting ongoing matches

--- src/test/groovy/com/sport/scores/ScoreBoardServiceTest.groovy
+++ src/test/groovy/com/sport/scores/ScoreBoardServiceTest.groovy
@@ -131,4 +131,27 @@ class ScoreBoardServiceTest extends Specification {
         scoreBoardService.matches.getAt(0).homeTeam.score == 3
         scoreBoardService.matches.getAt(0).awayTeam.score == 4
     }
+
+    def "matches should be sorted by total score and for equal total scores should be sorted by most recent started"() {
+        given:
+        scoreBoardService.matches.clear()
+        when:
+        Match m1 = startAndUpdateMatch("Mexico","Canada",0,5)
+        Match m2 = startAndUpdateMatch("Spain","Brazil",0,5)
+        Match m3 = startAndUpdateMatch("Germany","France",2,2)
+        Match m4 = startAndUpdateMatch("Uruguay","Italy",6,6)
+        Match m5 = startAndUpdateMatch("Argentina","Australia",3,1)
+        List<Match> sortedMatches = scoreBoardService.getOngoingMatchesOrdered()
+        then:
+        [m4, m2, m1, m5, m3] == sortedMatches
+    }
+
+    private Match startAndUpdateMatch(String homeTeamName, String awayTeamName, int homeTeamScore, int awayTeamScore){
+        Team team1 = Team.builder().name(homeTeamName).build()
+        Team team2 = Team.builder().name(awayTeamName).build()
+        Match match = Match.builder().homeTeam(team1).awayTeam(team2).build()
+        scoreBoardService.startMatch(match)
+        scoreBoardService.updateScore(match, homeTeamScore, awayTeamScore)
+        return match
+    }
 }
