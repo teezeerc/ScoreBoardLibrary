7168036ef4e6af643a6d20da53c5b2743bbce418
TZ <tomasz.zumbrzycki@gmail.com>
2024-04-28 18:28:09 +0200

    Tests for normal and egde cases for starting a match

--- src/test/groovy/com/sport/scores/ScoreBoardServiceTest.groovy
+++ src/test/groovy/com/sport/scores/ScoreBoardServiceTest.groovy
@@ -1,7 +1,11 @@
 package com.sport.scores
 
-
+import com.sport.scores.dict.TeamType
+import com.sport.scores.exception.InvalidMatchState
+import com.sport.scores.model.Match
+import com.sport.scores.model.Team
 import com.sport.scores.service.ScoreBoardService
+import com.sport.scores.service.ScoreBoardServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
@@ -10,10 +14,41 @@ import spock.lang.Specification
 class ScoreBoardServiceTest extends Specification {
 
     @Autowired(required = true)
-    private ScoreBoardService scoreBoardService;
+    private ScoreBoardServiceImpl scoreBoardService
 
     def "after context is loaded, beans are available"() {
         expect: "the scoreService is not null"
         scoreBoardService
+    }
+
+    def "after starting a match it should be added to matches collection"() {
+        when:
+        Team team1 = Team.builder().name("TeamA").type(TeamType.HOME).build()
+        Team team2 = Team.builder().name("TeamB").type(TeamType.AWAY).build()
+        Match match = Match.builder().homeTeam(team1).awayTeam(team2).build()
+        scoreBoardService.startMatch(match)
+        then:
+        scoreBoardService.matches.size() == 1
+    }
+
+    def "starting a match with two teams in the same role should throw exception"() {
+        when:
+        Team team1 = Team.builder().name("TeamA").type(TeamType.HOME).build()
+        Team team2 = Team.builder().name("TeamB").type(TeamType.HOME).build()
+        Match match = Match.builder().homeTeam(team1).awayTeam(team2).build()
+        scoreBoardService.startMatch(match)
+        then:
+        thrown InvalidMatchState
+    }
+
+    def "starting a match that was already started should throw exception"() {
+        when:
+        Team team1 = Team.builder().name("TeamA").type(TeamType.HOME).build()
+        Team team2 = Team.builder().name("TeamB").type(TeamType.AWAY).build()
+        Match match = Match.builder().homeTeam(team1).awayTeam(team2).build()
+        scoreBoardService.startMatch(match)
+        scoreBoardService.startMatch(match)
+        then:
+        thrown InvalidMatchState
     }
 }
