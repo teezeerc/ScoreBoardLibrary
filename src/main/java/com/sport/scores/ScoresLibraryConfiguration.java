7484d592e9d9f9cb0ed22cc1f6155c611dca528a
TZ <tomasz.zumbrzycki@gmail.com>
2024-04-28 18:13:11 +0200

    Configuration for ScoreBoardService bean

--- src/main/java/com/sport/scores/ScoresLibraryConfiguration.java
+++ src/main/java/com/sport/scores/ScoresLibraryConfiguration.java
@@ -1,5 +1,8 @@
 package com.sport.scores;
 
+import com.sport.scores.service.ScoreBoardService;
+import com.sport.scores.service.ScoreBoardServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ScoresLibraryConfiguration {
 
-
+    @Bean
+    ScoreBoardService getScoreService(){
+        return new ScoreBoardServiceImpl();
+    }
 }
